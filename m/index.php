b3a3ba518e00c54c46a4cc295c107848b4e6bfd4
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2012-06-14 20:34:52 -0300

    Login movil

---
+++ m/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>¡TuMovida!</title>
+</head>
+<body>
+	<div class=''>
+		<!--Inicio de sesión -->
+		<form action "user.php?login" method="POST">
+			<input type="text" name="mail" />
+			<input type="password" name="password" />s
+		</form>
+	</div>
+</body>
+</html>

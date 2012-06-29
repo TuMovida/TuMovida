089bfb8bdce082252bb8bb0c1dd07af7a40da237
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2012-06-29 14:49:54 -0300

    Corrección en destacados

--- inc/destacados.php
+++ inc/destacados.php
@@ -11,6 +11,17 @@ class destacados extends pagina
 			return false;
 		$return = array();
 		while ($destacado = mysql_fetch_assoc($this->q)){
+			if($destacado['Tipo'] == "Evento" && is_numeric($destacado['Enlace'])
+				&& $destacado['Enlace'] != 0){
+				$evento = new evento($destacado['Enlace']);
+				if($evento->getPromo()){
+					$destacado['Entradas'] = $evento->getPromo();
+				}else{
+					$destacado['Entradas'] = false;
+				}
+			}else{
+				$destacado['Entradas'] = false;
+			}
 			$return[] = $destacado;
 		}
 		return json_encode($return);
